766e8e0e73d48c84db84ad4ab5caa3dfad64bd5f
Pablo Estrada <pabloem@apache.org>
2021-09-27 14:25:55 -0700

    trying new dataset

--- sql/core/src/test/java/test/org/apache/spark/sql/JavaSparkStrategyOptimizationSuite.java
+++ sql/core/src/test/java/test/org/apache/spark/sql/JavaSparkStrategyOptimizationSuite.java
@@ -17,13 +17,12 @@
 
 package test.org.apache.spark.sql;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collections;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan;
+import org.apache.spark.sql.catalyst.rules.Rule;
 import org.apache.spark.sql.connector.catalog.InMemoryTableCatalog;
 import org.apache.spark.sql.execution.ExplainMode;
 import org.apache.spark.sql.execution.SparkPlan;
@@ -45,6 +44,9 @@ public class JavaSparkStrategyOptimizationSuite {
     @Override
     public Seq<SparkPlan> apply(LogicalPlan plan) {
       // If this SparkStrategy does not apply to the input plan, we must return an empty list.
+      // If this is a Join, and it joins on a column that we index, then...
+      // 1. Retrieve the full index for the column
+      // 2. Return a physical plan that uses the index to read the table
       return JavaConverters.asScalaIteratorConverter(
           Collections.<SparkPlan>emptyIterator()).asScala().toSeq();
     }
@@ -58,63 +60,52 @@ public class JavaSparkStrategyOptimizationSuite {
 
   @Before
   public void createTestTable() {
+    /*
+     * Dataset information: https://www.kaggle.com/olistbr/brazilian-ecommerce
+     */
     this.spark = new TestSparkSession();
     spark.conf().set("spark.sql.catalog.testcat", InMemoryTableCatalog.class.getName());
-    spark.sql("CREATE TABLE testcat.t (s string) USING foo");
-    Dataset<Row> ds = spark.read().parquet("src/test/resources/test-data/index-opt/premier_league.parquet");
-    ds.createOrReplaceTempView("premier_league_data");
-    /*
-     *  Schema of the parquet file with premier league information:
-     *
-     * For more detailed info: https://www.kaggle.com/irkaal/english-premier-league-results
-     *
-     *  <pyarrow._parquet.ParquetSchema object at 0x7ff147f62280>
-     * required group field_id=0 schema {
-     *   optional binary field_id=1 Season (String);
-     *   optional int64 field_id=2 DateTime (Timestamp(isAdjustedToUTC=true, timeUnit=microseconds, is_from_converted_type=false, force_set_converted_type=false));
-     *   optional binary field_id=3 HomeTeam (String);
-     *   optional binary field_id=4 AwayTeam (String);
-     *   optional int32 field_id=5 FTHG;
-     *   optional int32 field_id=6 FTAG;
-     *   optional binary field_id=7 FTR (String);
-     *   optional int32 field_id=8 HTHG;
-     *   optional int32 field_id=9 HTAG;
-     *   optional binary field_id=10 HTR (String);
-     *   optional binary field_id=11 Referee (String);
-     *   optional int32 field_id=12 HS;
-     *   optional int32 field_id=13 AS;
-     *   optional int32 field_id=14 HST;
-     *   optional int32 field_id=15 AST;
-     *   optional int32 field_id=16 HC;
-     *   optional int32 field_id=17 AC;
-     *   optional int32 field_id=18 HF;
-     *   optional int32 field_id=19 AF;
-     *   optional int32 field_id=20 HY;
-     *   optional int32 field_id=21 AY;
-     *   optional int32 field_id=22 HR;
-     *   optional int32 field_id=23 AR;
-     * }
-     */
+    Dataset<Row> customers = spark.read().parquet("src/test/resources/test-data/index-opt/olist_customers_dataset.csv.parquet");
+    Dataset<Row> orders = spark.read().parquet("src/test/resources/test-data/index-opt/olist_orders_dataset.csv.parquet");
+    customers.createOrReplaceTempView("customers");
+    orders.createOrReplaceTempView("orders");
   }
 
   @After
   public void dropTestTable() {
-    spark.sql("DROP TABLE testcat.t");
     spark.stop();
   }
 
   @Test
   public void testPlayWithSparkStrategy() throws Exception {
-    spark.extensions().injectPlannerStrategy(
+    spark.extensions()
+        .injectPlannerStrategy(
+            builder -> {
+              return new ExperimentingWithStrategy();
+            });
+
+    spark.extensions().injectOptimizerRule(
         builder -> {
-          return new ExperimentingWithStrategy();
+          return new Rule<LogicalPlan>() {
+            @Override
+            public LogicalPlan apply(LogicalPlan plan) {
+              return plan;
+            }
+          };
         }
     );
-    Dataset<Row> ds = spark.sql("SELECT HomeTeam, AwayTeam, COUNT(HomeTeam, AwayTeam) "
-        + "FROM premier_league_data WHERE FTHG > 0 "
-        + "GROUP BY HomeTeam, AwayTeam");
+    // Dataset<Row> ds = spark.sql("SELECT customer_id, order_status, COUNT(DISTINCT order_id) "
+    //     + "FROM orders WHERE order_estimated_delivery_date != 'aidiosmio' "
+    //     + "GROUP BY customer_id, order_status");
 
-    System.out.println("PABLOEM - HERE IS INFO ABOUT THE EXECUTION PLAN");
-    System.out.println(ds.queryExecution().explainString(ExplainMode.fromString("extended")));
+    // System.out.println("PABLOEM - HERE IS INFO ABOUT THE EXECUTION PLAN");
+    // System.out.println(ds.queryExecution().explainString(ExplainMode.fromString("extended")));
+
+    Dataset<Row> ds2 = spark.sql("SELECT c.customer_id, c.customer_state, o.order_status, COUNT(DISTINCT o.order_id) "
+        + "FROM customers as c JOIN orders as o ON c.customer_id = o.customer_id "
+        + "WHERE c.customer_state != 'Washington' AND o.order_status != 'oioioi' "
+        + "GROUP BY c.customer_id, c.customer_state, o.order_status");
+
+    System.out.println(ds2.queryExecution().explainString(ExplainMode.fromString("extended")));
   }
 }

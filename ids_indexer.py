0705b60b7d1de634280e1e4c71bc2d9aed77f6fb
albertoibm <betoman@gmail.com>
2021-09-30 17:24:58 -0500

    Check that list is not empty before indexing it. Get column name in args

--- ids_indexer.py
+++ ids_indexer.py
@@ -8,15 +8,22 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("-1", "--files1", help="Parquet files to index", type=str, nargs='*')
 parser.add_argument("-2", "--files2", help="Parquet files to index against", type=str, nargs='*')
+parser.add_argument("-c", "--column", help="Name of column to look for. If not given, will use first column of files1", type=str, default = None)
 
 
 args = parser.parse_args()
+
+if args.files1 == None or args.files2 == None:
+    parser.print_help()
+    exit()
+
+colid = args.column
 
 index = {}
 
 for f1 in args.files1:
     pf1 = pq.ParquetFile(f1)
-    col_id = pf1.schema.column(0).name
+    col_id = colid if colid != None else pf1.schema.column(0).name
     for crg in range(pf1.num_row_groups):
         rg1 = pf1.read_row_group(crg, columns = [col_id])
         for f2 in args.files2:
@@ -25,7 +32,9 @@ for f1 in args.files1:
                 rg2 = pf2.read_row_group(org, columns = [col_id])
                 cids = rg1.to_pandas()
                 ocids = rg2.to_pandas()
-                index[f"{f1},{crg};{f2},{org}"] = list(pd.merge(cids,ocids,how="inner")[col_id].values)
+                intersect = list(pd.merge(cids,ocids,how="inner")[col_id].values)
+                if len(intersect) > 0:
+                    index[f"{f1},{crg};{f2},{org}"] = intersect
 import json
 with open("index.json","w") as fopen:
     json.dump(index,fopen)
